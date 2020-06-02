7d1608fef4c2ccfba8e976386913f8a62b20b3da
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-06-02 00:23:09 -0700

    Updated app.get

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send(db.users);
+	res.send('it is working');
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
